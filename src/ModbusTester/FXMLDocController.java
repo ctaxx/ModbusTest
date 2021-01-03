6daab086ba01e3c52e30dead1f5b5138d989518b
ctaxx <bykov.stanislav.p@gmail.com>
2021-01-04 00:44:12 +0200

    integration of itemButtons and read-write regs

--- src/ModbusTester/FXMLDocController.java
+++ src/ModbusTester/FXMLDocController.java
@@ -8,23 +8,25 @@ package ModbusTester;
 import ModbusTester.device.ParserCSV;
 import ModbusTester.tasks.DataPackageWriter;
 import ModbusTester.utils.FileUtils;
-import static java.lang.Thread.sleep;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
+import static java.lang.Thread.sleep;
 
 /**
  * FXML Controller class
@@ -35,8 +37,11 @@ public class FXMLDocController implements Initializable {
 
     private static final double ITEMS_WIDTH = 180.0;
     public DataPackageWriter dataPackageWriter = new DataPackageWriter();
+
+    VBox itemsVBox;
+    TableView readWriteRegsTable;
+
     private boolean locked = false;
-    private boolean activeTask;
 
     @FXML
     private TextField ipField;
@@ -48,22 +53,18 @@ public class FXMLDocController implements Initializable {
     private BorderPane anchorPane;
 
     @FXML
-    private VBox itemsVBox;
-    
-    @FXML
     private VBox devicesVBox;
 
     @FXML
     private ProgressBar taskProgress;
-    
+
     @FXML
     private Button cancelButton;
-    
+
     @FXML
     private StackPane activeCenterStack;
 
-    ArrayList<Button> buttons;
-
+//    ArrayList<Button> buttons;
     /**
      * Initializes the controller class.
      *
@@ -81,15 +82,65 @@ public class FXMLDocController implements Initializable {
     }
 
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        if (buttons != null) {
-            return;
+    private void handleReadWriteRegsAction(ActionEvent event) {
+        if (readWriteRegsTable == null) {
+            readWriteRegsTable = new TableView();
+            readWriteRegsTable.setId("readWriteRegsTable");
+            
+            activeCenterStack.getChildren().add(readWriteRegsTable);
+        } else {
+            setNodeToFront("readWriteRegsTable");
         }
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+//        if (buttons != null) {
+//            return;
+//        }
 //        DoubleProperty barUpdater = new SimpleDoubleProperty(dataPackageWriter.progress);
 //        System.out.println("for binding" + dataPackageWriter.progress);
 //        taskProgress.progressProperty().bind(barUpdater);
 
+        if (itemsVBox == null) {
+            itemsVBox = new VBox();
+            itemsVBox.setId("itemsVBox");
+            itemsVBox.setPrefWidth(ITEMS_WIDTH);
+
+            for (int i = 0; i < dataPackageWriter.itemArrayList.size(); i++) {
+                PackageItem item = dataPackageWriter.itemArrayList.get(i);
+                Button tmpButton = new Button(item.name);
+                tmpButton.setMinWidth(ITEMS_WIDTH);
+                int[] tmpArr = item.paramArray.stream().mapToInt(e -> (int) e.address).toArray();
+                long[] tmpVal = item.paramArray.stream().mapToLong(e -> (long) e.value).toArray();
+                tmpButton.setOnAction(e -> {
+                    if (locked) {
+                        return;
+                    }
+                    locked = true;
+                    taskProgress.setVisible(true);
+                    cancelButton.setVisible(true);
+                    System.out.println(item.name + " clicked");
+                    dataPackageWriter.init();
+
+                    new Thread(() -> {
+                        dataPackageWriter.writeData(tmpArr, tmpVal);
+                        dataPackageWriter.disconnect();
+                        locked = false;
+                        taskProgress.setVisible(false);
+                        cancelButton.setVisible(false);
+                    }).start();
+                }
+                );
+                itemsVBox.getChildren().add(tmpButton);
+            }
+            activeCenterStack.getChildren().add(itemsVBox);
+
+            startProgressBarUpdater();
+        } else {
+            setNodeToFront("itemsVBox");
+        }
+        /*
         Thread progressUpdater = new Thread(() -> {
             while (true) {
 //                Platform.runLater(() -> taskProgress.setProgress(dataPackageWriter.progress));
@@ -103,41 +154,7 @@ public class FXMLDocController implements Initializable {
         });
         progressUpdater.setDaemon(true);
         progressUpdater.start();
-        buttons = new ArrayList<>();
-        itemsVBox.setPrefWidth(ITEMS_WIDTH);
-
-        for (int i = 0; i < dataPackageWriter.itemArrayList.size(); i++) {
-            PackageItem item = dataPackageWriter.itemArrayList.get(i);
-            Button tmpButton = new Button(item.name);
-            tmpButton.setMinWidth(ITEMS_WIDTH);
-            int[] tmpArr = item.paramArray.stream().mapToInt(e -> (int) e.address).toArray();
-            long[] tmpVal = item.paramArray.stream().mapToLong(e -> (long) e.value).toArray();
-            tmpButton.setOnAction(e -> {
-                if (locked) {
-                    return;
-                }
-                locked = true;
-                taskProgress.setVisible(true);
-                cancelButton.setVisible(true);
-                System.out.println(item.name + " clicked");
-                dataPackageWriter.init();
-                
-                new Thread(() -> {
-                    dataPackageWriter.writeData(tmpArr, tmpVal);
-                    dataPackageWriter.disconnect();
-                    locked = false;
-                    taskProgress.setVisible(false);
-                    cancelButton.setVisible(false);
-                }).start();
-            }
-            );
-            buttons.add(tmpButton);
-        }
-        buttons.stream().forEach((b) -> {
-            itemsVBox.getChildren().add(b);
-        });
-
-//        anchorPane.getChildren().add(itemsVBox);
+         */
     }
 
     @FXML
@@ -151,10 +168,51 @@ public class FXMLDocController implements Initializable {
         dataPackageWriter.setDevice(parser.getDevice());
         ipField.setText(parser.getDevice().ipAddress);
     }
-    
+
     @FXML
-    private void handleCancelAction(ActionEvent event){
+    private void handleCancelAction(ActionEvent event) {
         dataPackageWriter.enableWriterFlag = false;
+    }
+
+    private void setNodeToFront(String id) {
+        ObservableList<Node> children = activeCenterStack.getChildren();
+        if (children.size() > 0) {
+            Node topChild = children.get(children.size() - 1);
+            if (id.equals(topChild.getId())) {
+                System.out.println("Returned: top equals applicant");
+                return;
+            }
+
+            Node applicantChild = children
+                    .stream()
+                    .filter(f -> id.equals(f.getId()))
+                    .findFirst().orElse(null);
+
+            if (applicantChild != null) {
+                topChild.setVisible(false);
+                topChild.toBack();
+
+                applicantChild.toFront();
+                applicantChild.setVisible(true);
+            }
+        }
+    }
+
+//    TODO pause thread
+    private void startProgressBarUpdater() {
+        Thread progressUpdater = new Thread(() -> {
+            while (true) {
+//                Platform.runLater(() -> taskProgress.setProgress(dataPackageWriter.progress));
+                taskProgress.setProgress(dataPackageWriter.progress);
+                try {
+                    sleep(500);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(FXMLDocController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        progressUpdater.setDaemon(true);
+        progressUpdater.start();
     }
 
 }
