223592a7c2e9cfd2b1abc6efa3d99d911b423602
ctaxx <bykov.stanislav.p@gmail.com>
2021-01-15 17:32:03 +0200

    string256 on changing

--- src/ModbusTester/parameter/String256Parameter.java
+++ src/ModbusTester/parameter/String256Parameter.java
@@ -5,6 +5,8 @@
  */
 package ModbusTester.parameter;
 
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
@@ -22,8 +24,17 @@ public class String256Parameter extends Parameter {
     @Override
     public String getValueString(int[] dataArray) {
 //        TODO avoid creating a lot of String objects
-        return Arrays.stream(dataArray)
-                .mapToObj(v -> Character.toString((char) (v & 0xFF)) + Character.toString((char) (v >> 8)))
-                .collect(Collectors.joining());
+        byte [] resultBytes = new byte[dataArray.length * 2];
+        for (int i = 0; i < dataArray.length; i++) {
+            byte[] b = ByteBuffer.allocate(4).putInt(dataArray[i]).array();
+            resultBytes[i * 2] = b[3];
+            resultBytes[i * 2 + 1] = b[2];
+        }
+        
+        return new String(resultBytes, "win1251");
+
+//        return Arrays.stream(dataArray)
+//                .mapToObj(v -> Character.toString((char) (v & 0xFF)) + Character.toString((char) (v >> 8)))
+//                .collect(Collectors.joining());
     }
 }
